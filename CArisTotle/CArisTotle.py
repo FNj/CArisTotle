bbd62fb42da3cef45efeda9bf59a661c3af634ef
fnj <F.Navrkal@gmail.com>
2016-11-14 20:22:17 +0100

    Restructured into package

--- CArisTotle/CArisTotle.py
+++ CArisTotle/CArisTotle.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, current_app
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return current_app.send_static_file('stat.html')
 
 
 if __name__ == '__main__':
---
+++ CArisTotle/__init__.py
@@ -0,0 +1,1 @@
+from .CArisTotle import app
