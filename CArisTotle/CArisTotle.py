9affac48ace88349cc1a07d57f5ddbb74cc058ab
fnj <F.Navrkal@gmail.com>
2017-01-19 01:16:01 +0100

    Changed behaviour slightly

--- CArisTotle/CArisTotle.py
+++ CArisTotle/CArisTotle.py
@@ -1,12 +1,12 @@
-from flask import Flask
-from flask import url_for
+from flask import Flask, current_app
 
-app = Flask(__name__)
+app = Flask('CArisTotle')
 
 
 @app.route('/')
 def hello_world():
-    return url_for('static', filename='stat.html')
+    # return url_for('static', filename='stat.html')
+    return current_app.send_static_file('stat.html')
 
 if __name__ == '__main__':
     app.run()
